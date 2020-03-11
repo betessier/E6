4f8eabd66ae65d516c4f6a94e225a76cb3ebb8f2
betessier <62028135+betessier@users.noreply.github.com>
2020-03-11 13:55:31 +0100

    Add files via upload

---
+++ TpLaGrandeCourseDesLapins/TpLaGrandeCourseDesLapins/Lapin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TpLaGrandeCourseDesLapins {
+    class Lapin 
+    {
+        private string surnom;
+        private int age, position;
+        private static Random aleatoire = new Random();
+        public Lapin(string surnom, int age)
+        {
+            this.position = 0;
+            this.surnom = surnom;
+            this.age = age;
+        }
+        public void Avancer() 
+        {
+            int i = Lapin.aleatoire.Next(0,6);
+            this.position = this.position + i;
+        }
+        public string ToString() 
+        {
+           return string.Format("Nom : {0} Age : {1} position {2}", this.surnom,this.age,this.position);
+        }
+        public int Position()
+        {
+            return this.position;
+        }
+    }
+}
---
+++ TpLaGrandeCourseDesLapins/TpLaGrandeCourseDesLapins/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace TpLaGrandeCourseDesLapins {
+    class Program {
+        static void Main ()
+        {
+            List<Lapin> mesLapins = new List<Lapin>();
+            mesLapins.Add(new Lapin("Georges",45));
+            mesLapins.Add(new Lapin("michel",87));
+            mesLapins.Add(new Lapin("paul",1));
+            for (int i = 0; i < 100; i++) 
+            {
+                for (int j = 0; j < mesLapins.Count; j++) 
+                {
+                    mesLapins[j].Avancer();
+                }
+            }
+            for (int k = 0; k < mesLapins.Count; k++) {
+                Console.WriteLine(mesLapins[k].ToString());
+            }
+            Lapin gagnant = (Lapin)mesLapins[0];
+            foreach (Lapin lapin in mesLapins) {
+            }
+            Console.ReadLine();
+        }
+    }
+}
