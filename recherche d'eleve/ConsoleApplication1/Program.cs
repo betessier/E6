6da2ab15e07c21774d54c0817421c0f6371331dd
betessier <62028135+betessier@users.noreply.github.com>
2020-03-11 14:12:09 +0100

    Add files via upload

---
+++ recherche d'eleve/ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] eleves = new string[6] { "albert", "barnabé", "barth", "lisa", "homere", "marge" };
+            int[] notes = new int[6] {11,9,8,15,5,12};
+            Console.Write("Vous voulez la note de : ");
+            string nom = Console.ReadLine();
+            int position = PositionEleve(nom, eleves);
+            if (position == -1)
+            {
+                Console.WriteLine(nom + " n'existe pas ");
+            }
+            else
+            {
+                Console.WriteLine(nom + " à pour note "+notes[position]);
+            }
+            Console.ReadLine();
+        }
+        static int PositionEleve(string nom, string[] eleve)
+        {
+            int position = -1, i = 0;
+            bool valeur = false;
+            while (!valeur && i < eleve.Length)
+            {
+                if (eleve[i] == nom)
+                {
+                    position = i;
+                    valeur = true;
+                }
+                i++;
+            }
+
+            return position;
+        }
+
+    }
+}
---
+++ recherche d'eleve/ConsoleApplication14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication14 {
+    class Program {
+        static void Main(string[] args) {
+            string[] eleves = new string[6] { "albert", "barnabé", "barth", "lisa", "homere", "marge" };
+            Console.Write("Quel est le prénom à rechercher : ");
+            string nom = Console.ReadLine();
+            int position = PositionEleve(nom, eleves);
+            if (position == -1) {
+                Console.WriteLine(nom + " n'est pas dans le tableau ");
+            }
+            else {
+                Console.WriteLine(nom + " se trouve dans le poste {0} du tableau", position + 1);
+            }
+            Console.ReadLine();
+        }
+        static int PositionEleve(string nom, string[] eleve) {
+            int position = -1, i = 0;
+            bool valeur = false;
+            while (!valeur && i < eleve.Length) {
+                if (eleve[i] == nom) {
+                    position = i;
+                    valeur = true;
+                }
+                i++;
+            }
+
+            return position;
+        }
+
+    }
+}
