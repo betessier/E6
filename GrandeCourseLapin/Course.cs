9baac9e407926fd3e7aef0f8a86b5b8ba810cc37
betessier <62028135+betessier@users.noreply.github.com>
2020-03-11 13:50:23 +0100

    Add files via upload

---
+++ GrandeCourseLapin/Course.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace GrandeCourseLapin {
+    class Course {
+        private int distance;
+        private List<Lapin> participer;
+
+        public void Départ() {
+            for(int j = 0;j < this.distance;j++) {
+                for(int i = 0;i < this.participer.Count;i++) {
+                    this.participer[i].Avancer();
+                }
+            }
+        }
+
+        public Lapin Gagnant() {
+            Lapin gagnant = this.participer[0];
+            foreach(Lapin lapin in this.participer) {
+                if(lapin.Position > gagnant.Position) {
+                    gagnant = lapin;
+                }
+            }
+            return gagnant;
+        }
+
+        public Course(int distance) {
+            this.distance = distance;
+            this.participer = new List<Lapin>();
+        }
+
+        public void Add(Lapin nouveauParticipant) {
+            this.participer.Add(nouveauParticipant);
+        }
+
+        public void RemoveAt(int position) {
+            if(position > -1 && position < this.participer.Count) {
+                this.participer.RemoveAt(position);
+            }
+        }
+
+        public List<Lapin> GetParticiper() {
+            return this.participer;
+        }
+
+        public List<Lapin> Participer {
+            get {
+                return participer;
+            }
+        }
+
+        public Lapin this[int position] {
+            get {
+                return this.participer[position];
+            }
+        }
+        public int Count {
+            get {
+                return this.participer.Count;
+            }
+        }
+    }
+}
---
+++ GrandeCourseLapin/Lapin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandeCourseLapin {
+    class Lapin {
+        private static int comptage;
+        private static int pas;
+        private static Random aleatoire;
+        private string surnom;
+        private int age;
+        private int position;
+        private int dossard;
+
+        public static void PasEtStart(int start, int pasChoisis)
+        {
+            Lapin.comptage = start;
+            Lapin.pas = pasChoisis;
+        }
+
+        public Lapin(string surnom,int age) {
+            this.surnom = surnom;
+            this.age = age;
+            this.position = 0;
+            Lapin.aleatoire = new Random();
+            this.dossard = Lapin.comptage + Lapin.pas;
+            Lapin.comptage = Lapin.comptage + Lapin.pas;
+        }
+
+        public string Surnom {
+            get {
+                return this.surnom;
+            }
+
+            set {
+                this.surnom = value;
+            }
+        }
+
+        public int Age {
+            get {
+                return this.age;
+            }
+
+            set {
+                this.age = value;
+            }
+        }
+
+        public int Position {
+            get {
+                return this.position;
+            }
+        }
+
+        public void Avancer() {
+            this.position = this.position + Lapin.aleatoire.Next(0,5);
+        }
+        public string ToString() {
+            return string.Format("Un lapin nommé {0} agé de {1} ans et dont la position est {2} et a pour numéro de dossard {3}.",this.surnom, this.age, this.position, this.dossard);
+        }
+    }
+}
